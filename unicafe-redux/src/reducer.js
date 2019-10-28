6be300fc69dcd3895d7290772c3ddf342fffb0cc
jusalanen <jussialanen9@gmail.com>
2019-10-28 17:46:08 +0200

    lisää tehtävän 6.1 reducer & tests

--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -8,16 +8,16 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      return Object.assign({}, state, { good: state.good + 1 })
     case 'OK':
-      return state
+      return Object.assign({}, state, { ok: state.ok + 1 })
     case 'BAD':
-      return state
+      return Object.assign({}, state, { bad: state.bad + 1 })
     case 'ZERO':
-      return state
+      return Object.assign({}, state, { good: 0, ok: 0, bad: 0 })
     default: return state
   }
-  
+
 }
 
 export default counterReducer
